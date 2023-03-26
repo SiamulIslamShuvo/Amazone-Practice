11a25518015a6d4862e61c75ade68134eef3d347
SiamulIslamShuvo <siamulislamshuvo@gmail.com>
2023-03-26 21:00:59 +0600

    shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Componants/Header/Header";
+import Shop from "./Componants/Shop/shop";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Shop />
     </div>
   );
 }

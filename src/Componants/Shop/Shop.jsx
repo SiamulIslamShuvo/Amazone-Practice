5b097c73946d01a3c61ad6b557fd64be6754436f
SiamulIslamShuvo <siamulislamshuvo@gmail.com>
2023-03-26 21:48:47 +0600

    Products added

--- src/Componants/Shop/Shop.jsx
+++ src/Componants/Shop/Shop.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("/public/products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h3>Products Coming Soon</h3>
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
       </div>
       <div className="cart-container">
         <h3>Order SUmmary</h3>
